7cfc8496c42337db3945af4599fe695c250eff54
Jeremy Rans <jrans@vendasta.com>
2016-12-14 23:17:06 -0600

    day15 solutions

---
+++ day15/part2.py
@@ -0,0 +1,30 @@
+import itertools
+
+
+def rotate_discs(x):
+    for disc in discs:
+        disc[1] += x
+        disc[1] %= disc[0]
+
+
+for i in itertools.count():
+    discs = [
+        [17, 1],
+        [7, 0],
+        [19, 2],
+        [5, 0],
+        [3, 0],
+        [13, 5],
+        [11, 0]
+    ]
+    rotate_discs(i)
+    success = True
+    for j in xrange(len(discs)):
+        rotate_discs(1)
+        if discs[j][1] != 0:
+            success = False
+            break
+
+    if success:
+        print i
+        exit()
