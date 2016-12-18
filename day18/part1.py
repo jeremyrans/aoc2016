9fb6cc1c3b7fbb6d9de3a20b68714b3b3c6e22bc
Jeremy Rans <jrans@vendasta.com>
2016-12-17 23:56:48 -0600

    day18 solutions

---
+++ day18/part1.py
@@ -0,0 +1,25 @@
+input = ".^^^.^.^^^^^..^^^..^..^..^^..^.^.^.^^.^^....^.^...^.^^.^^.^^..^^..^.^..^^^.^^...^...^^....^^.^^^^^^^"
+row_count = 40
+rows = [input]
+
+
+def get_tile(l, c, r):
+    if l == '^' and c == '^' and r == '.':
+        return '^'
+    if l == '.' and c == '^' and r == '^':
+        return '^'
+    if l == '^' and c == '.' and r == '.':
+        return '^'
+    if l == '.' and c == '.' and r == '^':
+        return '^'
+    return '.'
+
+
+for i in xrange(row_count-1):
+    new_row = "".join([
+        get_tile(rows[i][j-1] if j-1 >= 0 else '.', rows[i][j], rows[i][j+1] if j+1 < len(input) else '.')
+        for j in xrange(len(input))
+    ])
+    rows.append(new_row)
+
+print sum(r.count('.') for r in rows)
